4fa980ee05f27489808430fde9c20f1e47316fa2
nangeel <mariangel.vega@ucr.ac.cr>
2023-05-27 03:24:49 -0600

    menu and all recipes page eliminated

--- dist/js/components/RecipeCard.js
+++ dist/js/components/RecipeCard.js
@@ -21,10 +21,26 @@ data() {
 
 template:
     /*html*/
-    `<div class="card border border-0">
+    `<div class="card border border-0 ">
         <img v-bind:src="image" class="card-img-top" alt="image-recipe">
         <div class="card-body">
         <h5 class="card-title">{{name}}</h5>
+        <button type="button" class="btn btn-primary"
+                            style="--bs-btn-padding-y: .5rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .7rem;">
+                            See More
+                        </button>
+                        <button type="button" class="btn btn-primary ms-3 " v-on:click="onClickLike()"
+                            style="--bs-btn-padding-y: .5rem; --bs-btn-padding-x: .2rem; --bs-btn-font-size: .9rem;">
+                            ❤️
+                        </button>
+                        <button type="button" class="btn btn-primary ms-3 " v-on:click="onClickLike()"
+                            style="--bs-btn-padding-y: .5rem; --bs-btn-padding-x: .2rem; --bs-btn-font-size: .9rem;">
+                            💔
+                        </button>
+                        <p>{{}}</p>
+                        
+                        
         </div>
+        
     </div>`
 })
--- dist/js/main.js
+++ dist/js/main.js
@@ -2,16 +2,53 @@ const app = Vue.createApp({
     data() {
         return {
             title: "Healthy Recipes",
+            logo: "./img/Recurso 2.webp",
+            image: "./img/img.inicio.webp",
             likes: 15,
+            tittleCategories: "Categories",
+            tittleMenu: "Menu",
+            categories: [
+                { id: 1, name:"All"},
+                { id: 2, name:"Breakfast"},
+                { id: 3, name:"Entrees"},
+                { id: 4, name:"Lunch"},
+                { id: 5, name:"Soups"},
+                { id: 6, name:"Desserts"},
+                { id: 7, name:"Drinks"},
+                
+            ],
+            difficulty: [
+                { id: 10, name: "Easy" },
+                { id: 20, name: "Medium" },
+                { id: 30, name: "Hard" },
+            ],
+            holidays: [
+                { id: 100, name: "All" },
+                { id: 200, name: "Birthday" },
+                { id: 300, name: "Father's day" },
+                { id: 400, name: "Mother's day" },
+                { id: 500, name: "Children's day" },
+                { id: 600, name: "Christmas" },
+                { id: 700, name: "Summer" },
+            ],
             recipes: [
-                { id: 1, name: "Pizza", image: "https://images.unsplash.com/photo-1581093457331-9f50a1c4b4e6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGl6emF8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80", likes: 15 },
+                { id: 101, name: "Toast", image: "./img/breakfast/toast.webp", likes: 15 },
+                { id: 102, name: "Toast", image: "./img/breakfast/toast.webp", likes: 16 },
+                { id: 103, name: "Toast", image: "./img/breakfast/toast.webp", likes: 17 },
+                { id: 104, name: "Toast", image: "./img/breakfast/toast.webp", likes: 18 }
             ],
 
         }
     },
-    mounted: function () {
-        console.log(this.recipes);
-    },
+
+    methods: {
+        onClickCategory(category) {
+            console.log("category ->" + category);
+        }
+
+    }
+
+
 
 
 })
