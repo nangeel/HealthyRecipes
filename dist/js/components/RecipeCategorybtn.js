1cbafb04a6b76bc5eb67fa7b064a144a7e63c602
nangeel <mariangel.vega@ucr.ac.cr>
2023-05-28 08:24:37 -0600

    recipe category button creation

---
+++ dist/js/components/RecipeCategorybtn.js
@@ -0,0 +1,15 @@
+app.component('recipe-category-btn', {
+    props: {
+        name: {
+            type: String,
+        },
+
+    },
+    data() {
+    },
+
+    template:
+    /*html*/
+    `<button class="dropdown-item">{{name}}</button>`
+})
+
