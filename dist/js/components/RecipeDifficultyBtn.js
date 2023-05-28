83cd8c2655445393263294cb83ecfe3d65d92fb8
nangeel <mariangel.vega@ucr.ac.cr>
2023-05-28 08:26:38 -0600

    recipe difficulty button creation

---
+++ dist/js/components/RecipeDifficultyBtn.js
@@ -0,0 +1,15 @@
+app.component('recipe-difficulty-btn', {
+    props: {
+        name: {
+            type: String,
+        },
+
+    },
+    data() {
+    },
+
+    template:
+    /*html*/
+    `<button class="dropdown-item">{{name}}</button>`
+})
+
