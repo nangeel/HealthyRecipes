ca2cda7416bfeb40d369027790120718f1ceb60b
nangeel <mariangel.vega@ucr.ac.cr>
2023-05-28 08:25:46 -0600

    recipe holiday button creation

---
+++ dist/js/components/RecipeHolidaysBtn.js
@@ -0,0 +1,15 @@
+app.component('recipe-holidays-btn', {
+    props: {
+        name: {
+            type: String,
+        },
+
+    },
+    data() {
+    },
+
+    template:
+    /*html*/
+    `<button class="dropdown-item">{{name}}</button>`
+})
+
